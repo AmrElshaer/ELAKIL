ab11dc3e615dd20f06c675a51de5e65057fc2d5c
Amr Elshaer <amrelsher07@gmail.com>
2021-09-02 19:19:38 +0200

    add login and register

---
+++ ELAKIL.UI/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ELAKIL.Business.Contexts;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ELAKIL.UI.Areas.Identity.IdentityHostingStartup))]
+namespace ELAKIL.UI.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
