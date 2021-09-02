279fcbfc6344461edc15c61ed2d6914da29a2110
xx7Ahmed7xx <ahmadayman24789@gmail.com>
2021-09-02 18:30:48 +0200

    Test by me

--- ELAKIL.UI/Startup.cs
+++ ELAKIL.UI/Startup.cs
@@ -22,6 +22,8 @@ namespace ELAKIL.UI
             Configuration = configuration;
         }
 
+        // Testing by Ahmed Mansour
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
