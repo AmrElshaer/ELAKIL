c606a689b64220af77048aa321256a71ed6a0a94
Amr Elshaer <amrelsher07@gmail.com>
2021-09-06 21:14:15 +0200

    add index and details in orders

---
+++ ELAKIL.Business/IService/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using ELAKIL.Business.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELAKIL.Business.IService
+{
+    public interface IOrderService
+    {
+        Task<Order> GetOrderAsync(int id);
+        Task<IEnumerable<Order>> GetOrdersAsync(DateTime? orderDate, DateTime? deliverDate, string status, int? code, string user);
+    }
+}
---
+++ ELAKIL.UI/Controllers/OrdersController.cs
@@ -0,0 +1,32 @@
+﻿using ELAKIL.Business.Common;
+using ELAKIL.Business.IService;
+using ELAKIL.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ELAKIL.UI.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IOrderService _orderService;
+
+        public OrdersController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        public async Task<IActionResult> Index(DateTime? orderDate,DateTime? deliverDate,OrderStatus? status,int? code,string user)
+        {
+            var orders = await _orderService.GetOrdersAsync(orderDate, deliverDate, status?.ToString(), code, user);
+            var orderSearch = new OrderSearchModel(orderDate,deliverDate,status,code,user,orders);
+            return View(orderSearch);
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+           return View(await _orderService.GetOrderAsync(id));
+        }
+    }
+}
