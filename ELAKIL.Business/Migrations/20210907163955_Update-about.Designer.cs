171ef7b53f1db7fdf0a4b22cda22f7f6b4b3709c
Amr Elshaer <amrelsher07@gmail.com>
2021-09-07 19:09:23 +0200

    add contact us

--- ELAKIL.Business/Migrations/20210907163955_Update-about.Designer.cs
+++ ELAKIL.Business/Migrations/20210907163955_Update-about.Designer.cs
@@ -4,14 +4,16 @@ using ELAKIL.Business.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ELAKIL.Business.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210907163955_Update-about")]
+    partial class Updateabout
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ELAKIL.UI/MailService.cs
@@ -0,0 +1,46 @@
+﻿using ELAKIL.Business.Common.Models;
+using ELAKIL.Business.IService;
+using ELAKIL.UI.Models;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELAKIL.UI
+{
+    public class MailService:IMailService
+    {
+        private readonly IOptions<MailSettings> _mailSettings;
+
+        public MailService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings;
+        }
+
+        public async Task SendAsync(MessageModel message)
+        {
+
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(_mailSettings.Value.Mail);
+            email.To.Add(MailboxAddress.Parse(message.To));
+            email.Subject = message.Subject;
+            var builder = new BodyBuilder();
+            builder.HtmlBody = message.Body;
+            email.Body = builder.ToMessageBody();
+            using (var smtp = new SmtpClient())
+            {
+                smtp.Connect(_mailSettings.Value.Host, _mailSettings.Value.Port, SecureSocketOptions.StartTls);
+                smtp.Authenticate(_mailSettings.Value.Mail, _mailSettings.Value.Password);
+                await smtp.SendAsync(email);
+                smtp.Disconnect(true);
+            }
+
+
+        }
+    }
+}
