9da864ebee170020c34027f08e48f4a35f435629
Hazem Essam <hazemkwita123@gmail.com>
2021-09-07 13:12:04 +0200

    Add about service

---
+++ ELAKIL.Business/Service/AboutService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ELAKIL.Business.IService;
+using ELAKIL.Business.Contexts;
+using ELAKIL.Business.Entities;
+using System.Threading.Tasks;
+
+namespace ELAKIL.Business.Service
+{
+    public class AboutService : IAboutService
+    {
+        private ApplicationDbContext context;
+        
+        public AboutService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<About> GetAboutAsync(int id)
+        {
+            About about = await context.Abouts.FindAsync(id);
+            if (about is null) 
+                throw new NullReferenceException($"About with id { id } not found");
+            return about;
+        }
+
+        public async Task<int> EditAboutAsync(int id, About updatedAbout)
+        {
+            About about = await context.Abouts.FindAsync(id);
+            if (about is null)
+                throw new NullReferenceException();
+            about.AboutUs = updatedAbout.AboutUs;
+            about.FaceBook = updatedAbout.FaceBook;
+            about.Twitter = updatedAbout.Twitter;
+            about.Whatsapp = updatedAbout.Whatsapp;
+            about.Number = updatedAbout.Number;
+            about.Image = updatedAbout.Image;
+            await context.SaveChangesAsync();
+            return about.Id;
+        }
+    }
+}
---
+++ ELAKIL.UI/Controllers/AboutController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ELAKIL.Business.IService;
+using ELAKIL.Business.Entities;
+
+namespace ELAKIL.UI.Controllers
+{
+    public class AboutController : Controller
+    {
+        private IAboutService aboutService;
+
+        public AboutController(IAboutService aboutService)
+        {
+            this.aboutService = aboutService;
+        }
+
+        // Show about page
+        [HttpGet]
+        public async Task<IActionResult> Index(int id = 1)
+        {
+            About about = await aboutService.GetAboutAsync(id);
+            return View(about);
+        }
+
+        // Show edit about page
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id = 1)
+        {
+            About about = await aboutService.GetAboutAsync(id);
+            return View(about);
+        }
+
+        // Edit about by id
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, About about)
+        {
+            id = await aboutService.EditAboutAsync(id, about);
+            return RedirectToAction("Index", new { id });
+        }
+    }
+}
