86b1d70d0644ed2cf762e8396701c815f72a8fc2
nada abd elftah <nadaabdalftah@std.mans.edu.eg>
2021-09-09 18:06:26 +0200

    made meals control and views

---
+++ ELAKIL.UI/Controllers/MealsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ELAKIL.Business.Contexts;
+using ELAKIL.Business.Entities;
+
+namespace ELAKIL.UI.Controllers
+{
+    public class MealsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MealsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Meals
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Meals.Include(m => m.Category);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Meals/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var meal = await _context.Meals
+                .Include(m => m.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (meal == null)
+            {
+                return NotFound();
+            }
+
+            return View(meal);
+        }
+
+        // GET: Meals/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id");
+            return View();
+        }
+
+        // POST: Meals/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Image,Price,Description,CategoryId")] Meal meal)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(meal);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", meal.CategoryId);
+            return View(meal);
+        }
+
+        // GET: Meals/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var meal = await _context.Meals.FindAsync(id);
+            if (meal == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", meal.CategoryId);
+            return View(meal);
+        }
+
+        // POST: Meals/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Image,Price,Description,CategoryId")] Meal meal)
+        {
+            if (id != meal.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(meal);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MealExists(meal.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", meal.CategoryId);
+            return View(meal);
+        }
+
+        // GET: Meals/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var meal = await _context.Meals
+                .Include(m => m.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (meal == null)
+            {
+                return NotFound();
+            }
+
+            return View(meal);
+        }
+
+        // POST: Meals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var meal = await _context.Meals.FindAsync(id);
+            _context.Meals.Remove(meal);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MealExists(int id)
+        {
+            return _context.Meals.Any(e => e.Id == id);
+        }
+    }
+}
