502cdc2304a296d31fc27492ccd668a352dec6f2
Amr Elshaer <amrelsher07@gmail.com>
2021-09-09 21:44:53 +0200

    add meal validator

---
+++ ELAKIL.Business/Validators/MealValidator.cs
@@ -0,0 +1,19 @@
+﻿using ELAKIL.Business.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELAKIL.Business.Validators
+{
+    public class MealValidator : AbstractValidator<Meal>
+    {
+        public MealValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty();
+            RuleFor(c => c.Price).NotEmpty();
+        }
+    }
+}
